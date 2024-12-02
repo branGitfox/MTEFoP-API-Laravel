357a08955f88cf368a8fc06d0fd34a666b86b8b8
BranGitfox <vixfgit@gmail.com>
2024-12-02 21:01:55 +0300

    feat: validing fields

--- app/Http/Controllers/CourrierController.php
+++ app/Http/Controllers/CourrierController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
-
+use Illuminate\Http\Request;
 
 class CourrierController extends Controller
 {
-    public function addDoc () {
-        return 'ok';
+    public function addDoc (Request $request) {
+        $request->validate([
+            'prov' => 'required',
+            'chrono' => 'required|unique:courriers',
+            'ref' => 'required',
+            'id_dir' => 'required|exists:dirs,id',
+            'motif' => 'required',
+            'caracteristique' => 'required',
+            'propr' => 'required',
+            'id_user' => 'required|exists:users',
+            'status' => 'required'
+        ]);
     }
 }
