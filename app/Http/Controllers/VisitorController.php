add2b887c94ce82897f50619a45107d9a0f7c66c
BranGitfox <vixfgit@gmail.com>
2024-12-20 11:37:00 +0300

    feat:  increment method done

--- app/Http/Controllers/VisitorController.php
+++ app/Http/Controllers/VisitorController.php
@@ -5,10 +5,23 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreVisitorRequest;
 use App\Http\Requests\UpdateVisitorRequest;
 use App\Models\Visitor;
+use Illuminate\Support\Facades\DB;
 
 class VisitorController extends Controller
 {
     public function increment(){
+        $find = DB::table('visitors')->where('v_id', 1)->first();
+        if(!empty($find)){
+            $increment = $find->nbr + 1;
+
+          DB::update('update visitors set nbr = ? where v_id = ?',[ $increment, 1]);
+          $new =DB::table('visitors')->where('v_id', 1)->first();
+         return $new->nbr;
+        }else{
+
+            return  Visitor::create(['nbr' => 0]);
+        }
+
         
     }
 }
