7c71d98a62a107b2d9fe333fd5b2e0398e2e9b0a
BranGitfox <vixfgit@gmail.com>
2024-12-23 21:34:56 +0300

    feat: adding fields to support migration file

--- app/Models/Support.php
+++ app/Models/Support.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Support extends Model
 {
-    //
+    protected $fillable = [
+        'email',
+        'objet',
+        'message'
+    ];
 }
--- database/migrations/2024_12_23_183126_create_supports_table.php
+++ database/migrations/2024_12_23_183126_create_supports_table.php
@@ -12,7 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('supports', function (Blueprint $table) {
-            $table->id();
+            $table->id('su_id');
+            $table->string('email');
+            $table->string('objet');
+            $table->text('message');
             $table->timestamps();
         });
     }
